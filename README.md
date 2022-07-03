a780f5614e5357154f9049f00f9f75029246a606
Magno Reis <maagnoreiz@gmail.com>
2022-07-03 02:18:00 -0300

    main

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Atlas
-## Control of known.
+
+## Control of known
 
 I need an local server to help me stay up-to-date and giving away of spreadsheets. So What I need:
 
 - User rank
-    - Only personal can edit or view personal data.
+  - Only personal can edit or view personal data.
 - Documentation
-    - An blog type to describe what happen that day. Journaling.
+  - An blog type to describe what happen that day. Journaling.
+  - Staff people about
---
+++ main.py
@@ -0,0 +1,16 @@
+# Importações
+from cgitb import text
+from tkinter import *
+# Criar Janela
+janela = Tk()
+# Criar Textos
+texto_orientacao = Label(janela, text="ATLAS")
+texto_orientacao.grid(column=0, row=0)
+texto_orientacao2 = Label(janela, text="Mapeamento de informações")
+texto_orientacao2.grid(column=0, row=1)
+
+# Botão Fechar
+botao_close = Button(janela, text="Fechar", command=janela.destroy)
+botao_close.grid(column=0, row=2)
+# Manter janela aberta
+janela.mainloop()

f8989eca86f03a1b0d6e5c3bdac916938cd19be6
Magno Reis <maagnoreiz@gmail.com>
2022-07-15 00:36:46 -0300

    up

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
+#Projeto Atlas por Magno Reis 2022
 # Importações
-from cgitb import text
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+def login_clicked():
+    """ callback when the login button clicked
+    """
+    msg = f'You entered user: {user.get()} and password: {password.get()}'
+    showinfo(
+        title='Information',
+        message=msg
+    )
+
 # Criar Janela
-janela = Tk()
+janela = tk.Tk()
 janela.title('ATLAS')
+janela.resizable(False, False)
+signin = ttk.Frame(janela)
+signin.pack(padx=10, pady=10, fill='x', expand=True)
+signin2 = ttk.Frame(janela)
+signin2.pack(padx=10, pady=10, fill='x', expand=True)
+
+#Guardando as variáveis
+user = tk.StringVar()
+password = tk.StringVar()
+
 # Criar Textos
-texto_orientacao = Label(janela, text="ATLAS")
-texto_orientacao.grid(column=0, row=0)
-texto_orientacao2 = Label(janela, text="Mapeamento de informações")
-texto_orientacao2.grid(column=0, row=1)
+texto_orientacao = ttk.Label(signin, text="ATLAS")
+texto_orientacao.pack(fill='x', expand=True)
+texto_orientacao2 = ttk.Label(signin, text="Mapeamento de informações")
+texto_orientacao2.pack(fill='x', expand=True)
 
 # Botão Fechar
-botao_close = Button(janela, text="Fechar", command=janela.destroy)
-botao_close.grid(column=0, row=2)
+botao_close = ttk.Button(signin2, text="Entrar", command=janela.destroy)
+botao_close.pack(fill='x', side='bottom')
+
+# Tela de login
+user_label = ttk.Label(signin2, text='Usuário:')
+user_label.pack(fill='y')
+user_entry = ttk.Entry(signin2, textvariable=user)
+user_entry.pack(fill='y', expand=False)
+user_entry.focus()
+password_label = ttk.Label(signin2, text='Senha:')
+password_label.pack(fill='y', expand=False)
+password_entry = ttk.Entry(signin2, textvariable=password)
+password_entry.pack(fill='y', expand=False)
 # Manter janela aberta
 janela.mainloop()

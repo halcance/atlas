5c5abdb4930ae00d72156bf6c242f62326a00633
Magno Reis <maagnoreiz@gmail.com>
2022-07-19 00:25:18 -0300

    Included SQL, everything works.

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 #Projeto Atlas por Magno Reis 2022
 # Importações
 
+import sqlite3
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo, showwarning
 
 def login_clicked():
-    if(user.get()=='magno' and password.get() =='master'):
-        showinfo(title='info', message='BEM VINDO SENHOR CRIADOR DESTE PROGRAMA MARAVILHOSO')
+    usuario = user.get()
+    senha = password.get()
+    with sqlite3.connect('atlas.db') as banco:
+        cur = banco.cursor()
+        sql = f"""
+            select 
+            *
+            from users
+            """    
+        cur.execute(sql)
+        resultados = cur.fetchall()
+        banco.commit()
+
+    validado = False
+    for resultado in resultados:
+        if usuario == resultado[1] and senha == resultado[2]:
+            validado = True
+    if validado == True:
+       user_logado()
     else:
         showwarning(title='ERRO', message='Usuário e Senha incorreto.')
+
+def user_logado():
+    janela.withdraw()
+    janela2 = tk.Toplevel()
+    janela2.title('PÁGINA PRINCIPAL')
+    janela_label = tk.Label(janela2, text="PÁGINA PRINCIPAL")
+    janela_label.grid(row=0,column=0,padx=10,pady=10,columnspan=4)
+
 # Criar Janela
 janela = tk.Tk()
 janela.title('ATLAS')
